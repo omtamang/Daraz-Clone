bfa8bb49c915b7005626ab6e4e489d18e0e48632
Om Tamanag <np03cs4s220426@heraldcollege.edu.np>
2024-09-18 14:10:44 +0545

    Added new dev

--- src/Components/api/ApiService.js
+++ src/Components/api/ApiService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const retrieveUsers = (userName) =>axios.get(`http://daraz-clone-api-springboot-production.up.railway.app/users/${userName}`)
+export const retrieveUsers = (userName) =>axios.get(`https://daraz-clone-api-springboot-production.up.railway.app/users/${userName}`)
 
-export const addUser = (userDetails) => axios.post(`http://daraz-clone-api-springboot-production.up.railway.app/user`, userDetails)
+export const addUser = (userDetails) => axios.post(`https://daraz-clone-api-springboot-production.up.railway.app/user`, userDetails)
 
-export const retrieveAllusers = () => axios.get("http://daraz-clone-api-springboot-production.up.railway.app/users")
+export const retrieveAllusers = () => axios.get("https://daraz-clone-api-springboot-production.up.railway.app/users")
 
-export const retriveCartOfUser = (id) => axios.get(`http://daraz-clone-api-springboot-production.up.railway.app/users/${id}/carts`)
+export const retriveCartOfUser = (id) => axios.get(`https://daraz-clone-api-springboot-production.up.railway.app/users/${id}/carts`)
